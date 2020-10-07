3ed551b08b75d6d3f790e3ce33e339c5c821b997
Yudistiro Wahyu <wahyuyudistiro@gmail.com>
2020-10-07 21:15:02 +0700

    laast

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,20 +3,14 @@ var app = new Vue({
     el: '#app',
     data:{
         content: '',
-        // message1: 'Hello World!',
-        // message2:'yes its me',
+  
         comments:['test'],
         value: 1,
-        // down: 6,
-        // int3: 10,
+     
         vote:0,
     },
 
-    // computed: {
-    //     sum: function(){
-    //         return this.int1 + this.int2;
-    //     }
-    // },
+  
     methods:{
 
         addComment:function(){
